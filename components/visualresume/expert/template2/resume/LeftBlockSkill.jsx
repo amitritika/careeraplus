21e549d45c8532be3923421a6c6ff8d361b3f9cc
parsai.amit <parsai.amit@gmail.com>
2020-05-28 01:20:09 -0400

    Template2 Mod

--- components/visualresume/expert/template2/resume/LeftBlockSkill.jsx
+++ components/visualresume/expert/template2/resume/LeftBlockSkill.jsx
@@ -18,13 +18,14 @@ const LeftBlockBullet = (props) =>{
   let borderT = (props.fac * .5).toString() + "px";
   let width1 = (props.fac * 65).toString() + "px";
   let left1 = (props.fac * 4).toString() + "px";
+  
 
   return (
     <div id = {id} style = {{height: `${height}`, width: `${width}`, position: `absolute`, top: `${top}`, left: `${left}`, color: `white`, fontFamily: `calibri`, fontSize: `${size}`, textAlign: `left`}}>
        <p style = {{margin: `0px`, padding: `${paddingP}`}}>{name}</p>
       <div style = {{position: `absolute`, width: `${width1}`, left: `${left1}`}}>
         <div style = {{position: `absolute`, width: `100%`, height: `${heightDiv}`, border: `${borderT} white solid`, borderRadius: `${r}`}}></div>
-        <div style = {{position: `absolute`, width: `${rating}`, height: `${heightDiv}`,zIndex: `2`, backgroundColor: `white`, borderRadius: `${r}`}}></div>
+        <div className = "skill-box" style = {{position: `absolute`, width: `${rating}`, height: `${heightDiv}`,zIndex: `2`, backgroundColor: `white`, borderRadius: `${r}`}}></div>
       </div>
       
     </div>
